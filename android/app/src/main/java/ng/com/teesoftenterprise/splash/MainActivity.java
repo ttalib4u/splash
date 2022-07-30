dddcf78f25df90789828aeae4226a3b729b6f762
Abdulmutalib Idris A <abdulmutalib.idris.a@gmail.com>
2022-07-30 14:58:51 +0100

    initial commit

---
+++ android/app/src/main/java/ng/com/teesoftenterprise/splash/MainActivity.java
@@ -0,0 +1,6 @@
+package ng.com.teesoftenterprise.splash;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/views/splash.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Container(
+            decoration: const BoxDecoration(color: Colors.blueGrey),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                flex: 2,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: Colors.white,
+                      radius: 70.0,
+                      child: Image.asset('assets/images/logo.png'),
+                    ),
+                    Padding(padding: EdgeInsets.only(top: 10.0)),
+                    Text(
+                      "Teesoft Enterprise",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 24.0,
+                          color: Colors.white),
+                    )
+                  ],
+                ),
+              ),
+              // Expanded(
+              //   flex: 1,
+              // child: Column(
+              //   mainAxisAlignment: MainAxisAlignment.center,
+              //   children: [
+              //     CircularProgressIndicator.adaptive(value: 3.0,),
+              //     Padding(padding: const EdgeInsets.only(top: 20.0))
+              //   ],
+              // ),)
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
